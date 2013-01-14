5f4bc25502d688b1cf2755421b39584aa61ab988
loc <xlocle@gmail.com>
2013-01-15 00:37:29 +0700

    xuanloc implemented pinching, triple_tapping, panning gesture handling for UIGraphView

--- Calculator/UIGraphView.m
+++ Calculator/UIGraphView.m
@@ -11,10 +11,21 @@
 #import "AxesDrawer.h"  
 
 @interface UIGraphView()
-
+@property (nonatomic) double scale;
+@property (nonatomic) CGPoint origin;
 @end
 
 @implementation UIGraphView
+
+- (void)setScale:(double)scale {
+    _scale = scale;
+    [self setNeedsDisplay];
+}
+
+- (void)setOrigin:(CGPoint)origin {
+    _origin = origin;
+    [self setNeedsDisplay];
+}
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -26,22 +37,66 @@
 }
 
 
+- (void)awakeFromNib {
+    self.scale = 10;
+    self.origin = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2);
+    
+    UIPinchGestureRecognizer *pinchGestureRegnizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(userDidPinch:)];
+    [self addGestureRecognizer:pinchGestureRegnizer];
+    
+    UIPanGestureRecognizer *panGestureRegnizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(userDidPan:)];
+    [self addGestureRecognizer:panGestureRegnizer];
+    
+    UITapGestureRecognizer *tapGestureRegnizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(userDidTap:)];
+    tapGestureRegnizer.numberOfTapsRequired = 3;
+    [self addGestureRecognizer:tapGestureRegnizer];
+
+}
+
+- (void)userDidPinch:(id)sender {
+    if ([sender isMemberOfClass:[UIPinchGestureRecognizer class]]) {
+        UIPinchGestureRecognizer *recognizer = sender;
+        
+        self.scale = self.scale * recognizer.scale;
+        
+        if (self.scale < 1) {
+            self.scale = 1;
+        } else if (self.scale > 100) {
+            self.scale = 100;
+        }
+        
+    }
+}
+
+- (void)userDidPan:(id)sender {
+    if ([sender isMemberOfClass:[UIPanGestureRecognizer class]]) {
+        UIPanGestureRecognizer *recognizer = sender;
+        CGPoint translation = [recognizer translationInView:self];
+        [recognizer setTranslation:CGPointZero inView:self];
+        self.origin = CGPointMake(self.origin.x + translation.x, self.origin.y + translation.y);
+    }
+}
+
+- (void)userDidTap:(id)sender {
+    if ([sender isMemberOfClass:[UITapGestureRecognizer class]]) {
+        UITapGestureRecognizer *recognizer = sender;
+        self.origin = [recognizer locationInView:self];
+    }
+}
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
     // Drawing code
     //[AxesDrawer drawAxesInRect:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)originAtPoint:CGPointMake(self.center.y , self.center.y) scale:10];
-    CGPoint coordinatesTranslation = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2);
-    double scale = 10;
-    
-    [AxesDrawer drawAxesInRect:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)originAtPoint:coordinatesTranslation scale:scale];
+    [AxesDrawer drawAxesInRect:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)originAtPoint:self.origin scale:self.scale];
 
     CGContextRef context = UIGraphicsGetCurrentContext();
-    for (double x = coordinatesTranslation.x * -1; x < coordinatesTranslation.x; x++)
+    for (double x = self.origin.x * -1; x < self.frame.size.width - self.origin.x; x++)
     {
-        double y = [self.delegate yCoordinateOfXCoordinate:x / scale];
-        CGContextFillRect(context, CGRectMake(coordinatesTranslation.x + x, coordinatesTranslation.y - scale * y, 1, 1));
+        double y = [self.delegate yCoordinateOfXCoordinate:x / self.scale];
+        CGContextFillRect(context, CGRectMake(self.origin.x + x, self.origin.y - self.scale * y, 1, 1));
 //        NSLog(@"x = %g, y = %g -> point.x = %g, point.y = %g", x, y, coordinateTranslation.x + scale * x, coordinateTranslation.y - scale * y);
     }
 
